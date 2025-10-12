6fb02d604025869642dacffe5394114f2c2121bc
Ryan Turner <ryan@turnrye.com>
2025-10-12 10:47:53 -0500

    header: add copyright header to missing files, update existing header to match new one

--- openrtx/include/fonts/symbols/Symbols5pt7b.h
+++ openrtx/include/fonts/symbols/Symbols5pt7b.h
@@ -1,3 +1,22 @@
+/*
+ * Copyright (C) 2020-2025 OpenRTX Contributors
+ *
+ * This file is part of OpenRTX.
+ *
+ * OpenRTX is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OpenRTX is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 // This is a generated file, please do not edit it! Use generate_symbols.py
 static const uint8_t Symbols5pt7bBitmaps[] PROGMEM = {
 0x00, //space
